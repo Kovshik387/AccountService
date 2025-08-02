41212a46643108f0f3a4ac4934a07dbfd8588338
Kovshik387 <fantokin03@mail.ru>
2025-08-02 12:22:56 +0300

    feat: add authorize and responses types

--- src/AccountService/Features/Transactions/TransactionController.cs
+++ src/AccountService/Features/Transactions/TransactionController.cs
@@ -2,11 +2,13 @@
 using AccountService.Features.Transactions.Commands.TransferTransaction;
 using AccountService.Features.Transactions.Queries.GetTransactionById;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AccountService.Features.Transactions;
 
 [ApiController]
+[Authorize]
 [Route("api/transactions")]
 public class TransactionController : ControllerBase
 {
@@ -20,13 +22,14 @@ public class TransactionController : ControllerBase
     /// <param name="cancellationToken">Токен отмены</param>
     /// <returns></returns>
     [HttpPost]
-    [ProducesResponseType(StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(MbResult<CreateTransactionResponse>), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(MbResult),StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> CreateTransaction([FromBody] CreateTransactionCommand command,
         CancellationToken cancellationToken)
     {
         var result = await _mediator.Send(command, cancellationToken);
-        return CreatedAtAction(nameof(CreateTransaction), new { id = result.Id }, null);
+        return CreatedAtAction(nameof(CreateTransaction), new { id = result.Id },
+            MbResult<CreateTransactionResponse>.SuccessResult(result));
     }
     /// <summary>
     /// Получить транзакцию по Id
@@ -34,14 +37,16 @@ public class TransactionController : ControllerBase
     /// <param name="id">Уникальный идентификатор транзакции</param>
     /// <param name="cancellationToken">Токен отмены</param>
     /// <returns></returns>
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(MbResult<GetTransactionByIdQueryResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(MbResult), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(MbResult), StatusCodes.Status404NotFound)]
     [HttpGet]
     [Route("{id:guid}")]
     public async Task<IActionResult> GetTransactionById(Guid id, CancellationToken cancellationToken)
     {
-        var result = await _mediator.Send(new GetTransactionByIdQuery { Id = id }, cancellationToken);
-        return Ok(result);
+        var result = await _mediator.Send(new GetTransactionByIdQuery { Id = id },
+            cancellationToken);
+        return Ok(MbResult<GetTransactionByIdQueryResponse>.SuccessResult(result));
     }
     /// <summary>
     /// Произвести трансфер между счетами
@@ -49,15 +54,15 @@ public class TransactionController : ControllerBase
     /// <param name="command">Тело запроса</param>
     /// <param name="cancellationToken">Токен отмены</param>
     /// <returns></returns>
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(MbResult<TransferTransactionResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(MbResult), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(MbResult), StatusCodes.Status400BadRequest)]
     [HttpPost]
     [Route("transfer")]
     public async Task<IActionResult> TransferTransaction([FromBody] TransferTransactionCommand command,
         CancellationToken cancellationToken)
     {
         var result = await _mediator.Send(command, cancellationToken);
-        return Ok(result);
+        return Ok(MbResult<TransferTransactionResponse>.SuccessResult(result));
     }
 }
